ccc1683caa0ccb0f058459511d6951f4b17cb548
onetrueandrew <43832169+onetrueandrew@users.noreply.github.com>
2021-04-02 19:39:26 -0400

    Create services.conf.py

---
+++ services.conf.py
@@ -0,0 +1,14 @@
+# SAMPLE SERVICES FILE
+#
+# format as follows:
+#service_name address priority
+#
+# space as delimiter
+# commented lines are ignored
+codenames http://10.0.0.2:9095 1
+sonarr http://10.0.0.2:8989 2
+transmission http://10.0.0.2:9092 2
+changedetection http://10.0.0.2:5001 3
+lighttpd https://my.domain/testing_text.txt 5
+bazarr http://10.0.0.2:6767 2
+jellyfin http://10.0.0.2:8096 2
